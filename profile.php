53ceb099ca7ea624352394fdb095d9e1abe357be
Varun Kumar <imvarun@gmail.com>
2011-09-02 12:05:04 -0700

    profie update

--- profile.php
+++ profile.php
@@ -3,4 +3,5 @@
 	include "chkcookie.php";
 	include "header.php";
 ?>
+<div id = left>
 <img src = "<?php echo $display_picture; ?>" alt = "<?php echo  $username; ?>" class = "profile_pic big_thumbnail" />
