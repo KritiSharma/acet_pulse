7810308d462b68af7b4d0c3770a85851849f0613
Varun Kumar <imvarun@gmail.com>
2011-09-22 15:47:34 +0530

    mycommit

--- profile.php
+++ profile.php
@@ -3,5 +3,4 @@
 	include "chkcookie.php";
 	include "header.php";
 ?>
-<div id = left>
 <img src = "<?php echo $display_picture; ?>" alt = "<?php echo  $username; ?>" class = "profile_pic big_thumbnail" />
